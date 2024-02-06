5d66e0d213d76f928ca3332c3d81dd58ef8e3231
= <=>
2024-02-06 15:30:48 +0300

    navigation added

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,17 +1,29 @@
 import Link from 'next/link';
 import React from 'react';
+import { IoBugSharp } from 'react-icons/io5';
 
 const NavBar = () => {
+  const links = [
+    { label: 'Dashboard', href: '/' },
+    { label: 'Issues', href: '/issues' },
+  ];
+
   return (
-    <nav>
-      <Link href="/">logo</Link>
-      <ul>
-        <li>
-          <Link href="/">Dashboard</Link>
-        </li>
-        <li>
-          <Link href="/issues">Issues</Link>
-        </li>
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <IoBugSharp />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              href={link.href}
+              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
