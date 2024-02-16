5883878c784b14e8dab666d43c891519a17c67c5
= <=>
2024-02-16 20:55:17 +0300

    fetch assignee user

--- app/api/auth/authOptions.ts
+++ app/api/auth/authOptions.ts
@@ -11,7 +11,7 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  session: {
-    strategy: 'jwt',
-  },
+  // session: {
+  //   strategy: 'jwt',
+  // },
 };
