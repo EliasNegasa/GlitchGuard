ea602447d51e033ed1cbf18c6879bce4706d96ef
= <=>
2024-02-23 22:17:33 +0300

    make column sortable

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,15 +1,27 @@
 import { StatusBadge } from '@/app/components';
 import prisma from '@/prisma/client';
+import { Issue, Status } from '@prisma/client';
 import { Container, Table, Text } from '@radix-ui/themes';
 import Link from 'next/link';
 import IssueActions from './IssueActions';
-import { Status } from '@prisma/client';
+import { ArrowUpIcon } from '@radix-ui/react-icons';
 
 interface Props {
   searchParams: {
     status: Status;
+    orderBy: keyof Issue;
   };
 }
+
+const columns: { label: string; value: keyof Issue; className?: string }[] = [
+  { label: 'Title', value: 'title' },
+  { label: 'Status', value: 'status', className: 'hidden md:table-cell' },
+  {
+    label: 'Created at',
+    value: 'createdAt',
+    className: 'hidden md:table-cell',
+  },
+];
 
 const IssuePage = async ({ searchParams }: Props) => {
   const issues = await prisma.issue.findMany({
@@ -25,13 +37,26 @@ const IssuePage = async ({ searchParams }: Props) => {
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Title</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Status
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Created at
-            </Table.ColumnHeaderCell>
+            {columns.map((column) => (
+              <Table.ColumnHeaderCell
+                key={column.value}
+                className={column?.className}
+              >
+                <Link
+                  href={{
+                    query: {
+                      ...searchParams,
+                      orderBy: column.value,
+                    },
+                  }}
+                >
+                  {column.label}
+                </Link>
+                {column.value === searchParams.orderBy && (
+                  <ArrowUpIcon className="inline" />
+                )}
+              </Table.ColumnHeaderCell>
+            ))}
           </Table.Row>
         </Table.Header>
         <Table.Body>
